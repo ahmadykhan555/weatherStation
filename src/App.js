0ae697819d9c966f6c84313b728421f4285e06e4
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2019-06-07 21:31:41 +0500

    Application Entry point.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import WeatherStation from './components/WeatherStation/WeatherStation';
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherStation></WeatherStation>
+    </div>
+  );
+}
+
+export default App;
