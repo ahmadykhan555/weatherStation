cba91fd956deb3338d85024aa3fafdb7f7f55fcd
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2019-06-07 21:33:00 +0500

    Selected city card component

---
+++ src/components/SelectedCityCard/SelectedCityCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './SelectedCityCard.css';
+import WeatherByDay from '../WeatherByDay/WeatherByDay';
+
+const SelectedCityCard = () => {
+    return(
+        <div className="selected-city-card">
+            <h4 className="selected-city-card__city-name">Paris, France</h4>
+            <h5 className="time-now">9:30pm</h5>
+            <h5 className="date">30 May</h5>
+            <h6 className="day">Thursday</h6>
+            <div className="weather-status-icon"></div>
+            <h6 className="temperature-now">30c</h6>
+            <div className="forecast-for-week">
+                <WeatherByDay/>
+                <WeatherByDay/>
+                <WeatherByDay/>
+                <WeatherByDay/>
+            </div>
+        </div>
+    );
+}
+
+export default SelectedCityCard;
