7b601006647dc61a3f5ed79a04b306bd3b0604ac
Ahmad Yar Khan <ayk.digital@gmail.com>
2019-06-11 22:55:08 +0500

    Added selected city card overlay and removed hexagon hover animation

--- src/components/SelectedCityCard/SelectedCityCard.js
+++ src/components/SelectedCityCard/SelectedCityCard.js
@@ -6,17 +6,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const SelectedCityCard = () => {
     return(
         <div className="selected-city-card">
-            <h4 className="selected-city-card__city-name">Paris, France</h4>
-            <h5 className="time-now">9:30pm</h5>
-            <h5 className="date">30 May</h5>
-            <h6 className="day">Thursday</h6>
-            <FontAwesomeIcon icon="coffee"/>
-            <h6 className="temperature-now">30c</h6>
-            <div className="forecast-for-week">
-                <WeatherByDay/>
-                <WeatherByDay/>
-                <WeatherByDay/>
-                <WeatherByDay/>
+            <div className="selected-city-card__content">
+                <h4 className="selected-city-card__city-name text-item">Paris, France</h4>
+                <h5 className="time-now text-item">9:30pm</h5>
+                <h5 className="date text-item">30 May</h5>
+                <h6 className="day text-item">Thursday</h6>
+                <FontAwesomeIcon icon="coffee"/>
+                <h6 className="temperature-now">30c</h6>
+                <div className="forecast-for-week">
+                    <WeatherByDay/>
+                    <WeatherByDay/>
+                    <WeatherByDay/>
+                    <WeatherByDay/>
+                </div>
             </div>
         </div>
     );
