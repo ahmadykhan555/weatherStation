c3a1f1b851e3d3a8d38c9091f841757ca63865ff
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2019-07-14 03:18:43 +0500

    Avoid usage of divs

--- src/components/Hexagon/Hexagon.js
+++ src/components/Hexagon/Hexagon.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './Hexagon.css';
 
-const Hexagon = (props) => {
+const hexagon = (props) => {
     return(
         <div className="cell">
             <div className="hexagon"></div>
             <div className="weather-at-glance">
-                <div className="city-name">{props.cityName}</div>
-                <div className="weather-now">Sunny</div>
-                <div className="temperature">30c</div>
+                <h4 className="city-name">{props.cityName}</h4>
+                <h5 className="weather-now">Sunny</h5>
+                <h3 className="temperature">30c</h3>
             </div>
         </div>
     );
 }
 
-export default Hexagon;
+export default hexagon;
