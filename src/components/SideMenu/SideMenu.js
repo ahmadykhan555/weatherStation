84a2c3a6e15d938946b94e726efb41a68ef7a3a5
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2019-07-14 16:56:13 +0500

    Style sidemenu with sass

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import { FaHome, FaUserCircle, FaCog } from 'react-icons/fa';
 import { MdLocationOn } from 'react-icons/md';
-import './SideMenu.css';
+import './SideMenu.scss';
 const sideMenu = () => {
     return(
         <div className="side-menu">
-            <button className="side-menu-tab side-menu-tab-home tab-active">
+            <button className="side-menu__tab side-menu__tab--home tab-active">
                 <FaHome/>
             </button>
-            <button className="side-menu-tab side-menu-tab-location">
+            <button className="side-menu__tab side-menu__tab--location">
                 <MdLocationOn/>
             </button>
-            <button className="side-menu-tab side-menu-tab-profile">
+            <button className="side-menu__tab side-menu__tab--profile">
                 <FaUserCircle/>
             </button>
-            <button className="side-menu-tab side-menu-tab-settings">
+            <button className="side-menu__tab side-menu__tab--settings">
                 <FaCog/>
             </button>
         </div>
